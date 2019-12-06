d4c16884eb419d0e9f09f3b4ceac013df23bd567
Eric Catlin <ecatlin@starbucks.com>
2019-12-06 09:07:48 -0800

    feat: main exporting 2nd function of destructured sentence parts

--- main/index.js
+++ main/index.js
@@ -3,17 +3,25 @@ const { hash } = require("../hash");
 const { nouns, adverbs, adjectives, verbs } = require("../vocabulary");
 
 const getNamedId = (id, namespace) => {
-  const seededUUID = uuid(''+id, namespace);
+  const { number, adjective, noun, verb, adverb } = getNameIdParts(
+    id,
+    namespace
+  );
+  return `${number} ${adjective} ${noun} ${verb} ${adverb}`;
+};
+
+const getNameIdParts = (id, namespace) => {
+  const seededUUID = uuid("" + id, namespace);
   const idParts = seededUUID.split("-");
   const hashes = idParts.map(i => hash(i));
-  
+
   const number = (hash(seededUUID) % 100) + 1;
 
   const noun = nouns[hashes[0] % nouns.length];
   const adverb = adverbs[hashes[1] % adverbs.length];
   const adjective = adjectives[hashes[2] % adjectives.length];
   const verb = verbs[hashes[3] % verbs.length];
-  return `${number} ${adjective} ${noun} ${verb} ${adverb}`;
+  return { number, adjective, noun, verb, adverb };
 };
 
-module.exports = { getNamedId };
+module.exports = { getNamedId, getNameIdParts };
--- main/test.js
+++ main/test.js
@@ -1,8 +1,17 @@
-const { getNamedId } = require("./index");
+const { getNamedId, getNameIdParts } = require("./index");
 
 test("Generates random name", () => {
   const value = getNamedId();
   expect(value).not.toBeNull();
+});
+
+test("Generates name parts", () => {
+  const { number, adjective, noun, verb, adverb } = getNameIdParts();
+  expect(number).not.toBeNull();
+  expect(adjective).not.toBeNull();
+  expect(noun).not.toBeNull();
+  expect(verb).not.toBeNull();
+  expect(adverb).not.toBeNull();
 });
 
 test("Generates random names", () => {
@@ -19,3 +28,19 @@ test("Generates random names", () => {
 
   expect(values.length).toEqual(unique.length);
 });
+
+
+test("Validate name parts have equal distribution", () => {
+    const tests = 1000;
+    const values = [];
+  
+    for (let i = 0; i < tests; i++) {
+      values.push(getNameIdParts("hello world" + i));
+    }
+  
+    const unique = values.filter(
+      (value, index, self) => self.indexOf(value) === index
+    );
+  
+    expect(values.length).toEqual(unique.length);
+  });
